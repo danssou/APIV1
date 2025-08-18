64a77c66799c41197d954a5f6b6b0713f3ef25e2
Kokouvi <brightdanssou@gmail.com>
2025-08-18 23:44:29 +0200

    Creation of user and authentication routes

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import { PORT } from './config/env.js';
+
 
 
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send('Welcome to the subscription Tracker API')
+})
+
+app.listen(PORT, () => {
+    console.log("Server started at http://localhost:" + PORT);
+})
+
+
+export default app;
---
+++ routes/user.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get('/', (req,res) => res.send({title: 'GET all user'}))
+userRouter.get('/:id', (req,res) => res.send({title: 'GET user details '}))
+userRouter.post('/', (req,res) => res.send({title: 'CREATE user'}))
+userRouter.put('/:id', (req,res) => res.send({title: 'UPDATE user details'}))
+userRouter.delete('/:id', (req,res) => res.send({title: 'DELETE user details'}))
+
+
+export default userRouter;
