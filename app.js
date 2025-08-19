ec15b6798c648892f324effe222de2f06f35e845
Kokouvi <brightdanssou@gmail.com>
2025-08-19 23:54:47 +0200

    Adding subscription routes

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import { PORT } from './config/env.js';
 
+import subscriptionRouter from './routes/subscription.routes.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 
 
 const app = express();
+// creating route end point
+
+// api/v1/auth/sign-in
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+
+
 
 app.get("/", (req, res) => {
     res.send('Welcome to the subscription Tracker API')
