78f0877673a3079645f9df9eaad52914253f6e26
Kokouvi <brightdanssou@gmail.com>
2025-08-21 23:49:21 +0200

    Implement JWT user creation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,9 @@
 import mongoose from "mongoose"
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import {JWT_EXPIRES_IN, JWT_SECRET} from '../config/env.js'
+
+import User from '../models/user.models.js'
 
 export const signUp = async (req, res, next) => {
     const session = await mongoose.startSession();
@@ -7,6 +12,40 @@ export const signUp = async (req, res, next) => {
 
     try {
         // logic to create a new user
+
+        const {name, email, password} = req.body;
+
+        //  check if a user already exists
+        const existingUser = await User.findOne({email});
+
+        if (existingUser) {
+            const error = new Error('User already exists');
+            error.statusCode = 409;
+            throw error;
+        }
+
+        //  hash the password of the user
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUsers = await User.create([{name, email, password: hashedPassword}], {session});
+
+        const token = await jwt.sign({userId: newUsers[0]._id}, JWT_SECRET, {expiresIn:JWT_EXPIRES_IN}  )
+
+
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success : true,
+            message: 'User created successfully',
+            data : {
+                token,
+                user: newUsers[0],
+            }
+        })
+
     } catch (error) {
         await session.abortTransaction();
         session.endSession();
